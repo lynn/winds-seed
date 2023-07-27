30fa7a72f758380b0e9b5ffd4eff89b62f5ed7d8
Lynn <lynn@foldr.moe>
2023-07-27 19:10:04 +0200

    Refine story dump format

--- main.py
+++ main.py
@@ -2,61 +2,11 @@ from dataclasses import dataclass
 from typing import Dict, List
 import os.path
 
+import story
+
 
 def edit(byte_array: bytearray, index: int, contents: bytes):
     byte_array[index : index + len(contents)] = contents
-
-
-cc = {
-    0x4: "<NL>",
-    0x5: "<Next>",
-    0x8: "<Color=",
-    0xFF: "<End>\n",
-    0xB: "<LS=",
-    0xC: "<RS=",
-    0xD: "<LB>",
-    0xE: "<RB>",
-}
-
-speaker = {
-    0x0: "Mina",
-    0x1: "Otto",
-}
-
-
-def dump_story(story, table):
-    dump = []
-    p = 0x288
-    while p < len(story):
-        b = story[p]
-        if b == 0x20:
-            c = " "
-            p += 1
-        if b < 0x21 or b >= 0xF0:
-            c = cc.get(b, "<%02x>" % b)
-            p += 1
-            if c == "<Color=":
-                c += "%02x" % story[p] + ">"
-                p += 1
-            if "S=" in c:
-                c += speaker.get(story[p], "%02x" % story[p]) + ">"
-                p += 1
-        elif 0x21 <= b <= 0x7F:
-            c = table[b - 0x21]
-            p += 1
-        elif 0xA1 <= b <= 0xDF:
-            c = table[b - 0x20 - 0x21]
-            p += 1
-        else:
-            try:
-                c = story[p : p + 2].decode("cp932")
-            except UnicodeDecodeError as e:
-                print("???", hex(p))
-                raise e
-            p += 2
-        dump.append(c)
-    with open("story.txt", "w") as of:
-        of.write("".join(dump))
 
 
 @dataclass
@@ -116,40 +66,41 @@ if __name__ == "__main__":
         )
         size = (fcp_file[i + 17] << 8) + fcp_file[i + 16]
         contents = fcp_file[address : address + size]
+        # print(name, contents.count(b"\x0d\x0b"))
         if name == b"STORY.DAT":
-            story = contents
-            edit(fcp_file, address + 0x288, b"\x0d\x0b\x00We made it, Otto!\x05\xff")
+            story_contents = contents
+            edit(fcp_file, address + 0x288, b"\x0d\x0b\x00\x0a\x02\x07\x0a")
             # with open(name.decode(), "wb") as of:
             #    of.write(contents)
         i += 18
 
-    dump_story(story, table)
+    story.dump(story_contents, table)
 
     patched = rom[:]
 
     drbios = fs["DRBIOS.COM"]
     bios_data = bytearray(drbios.data)
 
-    # halfwidth text
-    edit(
-        bios_data,
-        0x9C7,
-        b"\x30\xE4\x2C\x40\xD0\xF8\x04\x40\x86\xE0\x66\x89\xC2\x90\x90\x90\x90\x90\x90\x90\x90\x90",
-    )
-    # nop out the cursor shl instruction
-    edit(bios_data, 0xA56, b"\x90\x90")
-    # make the text wrap at the end of the screen, not halfway through
-    edit(bios_data, 0xA4D, b"\x90")
+    if False:
+        # halfwidth text
+        edit(
+            bios_data,
+            0x9C7,
+            b"\x30\xE4\x2C\x40\xD0\xF8\x04\x40\x86\xE0\x66\x89\xC2\x90\x90\x90\x90\x90\x90\x90\x90\x90",
+        )
+        # nop out the cursor shl instruction
+        edit(bios_data, 0xA56, b"\x90\x90")
+        # make the text wrap at the end of the screen, not halfway through
+        edit(bios_data, 0xA4D, b"\x90")
+        patched[drbios.start : drbios.end] = bios_data
 
-    ws = fs["WS.COM"]
-    ws_data = bytearray(ws.data)
-    # left window cursor
-    edit(ws_data, 0x2532, b"\x0e")
-    # right window cursor
-    edit(ws_data, 0x25B5, b"\x30")
-
-    patched[drbios.start : drbios.end] = bios_data
-    patched[ws.start : ws.end] = ws_data
+        ws = fs["WS.COM"]
+        ws_data = bytearray(ws.data)
+        # left window cursor
+        edit(ws_data, 0x2532, b"\x0e")
+        # right window cursor
+        edit(ws_data, 0x25B5, b"\x30")
+        patched[ws.start : ws.end] = ws_data
 
     wsdata = fs["WSDATA.FCP"]
     patched[wsdata.start : wsdata.end] = fcp_file
---
+++ story.py
@@ -0,0 +1,100 @@
+cc = {
+    0x03: "<Something>",
+    0x07: "<Else=",
+    # Newline
+    0x04: "<NL>",
+    # Prompt for next message
+    0x05: "<Next>\n",
+    # Set text color
+    0x08: "<Color=",
+    # End text routine
+    0xFF: "<End>\n\n",
+    # Size
+    0x0a: "<Size=",
+    # Set left speaker sprite
+    0x0B: "<LS=",
+    # Set right speaker sprite
+    0x0C: "<RS=",
+    # Use left box
+    0x0D: "<LB>",
+    # Use right box
+    0x0E: "<RB>",
+}
+
+speaker = {
+    0x00: "Mina",
+    0x01: "Otto",
+    0x02: "Gray",
+    0x03: "TownMan",
+    0x04: "TownLady",
+    0x05: "TownKid",
+    0x07: "MinaInn",
+    0x09: "Innkeeper",
+    0x0a: "InnGuest",
+    0x0b: "CurryEater",
+    0x0c: "WindsSeed",
+    0x0e: "GodAngry",
+    0x0f: "Mickey",
+    0x10: "Donald",
+    0x11: "KronkCharge",
+    0x12: "God",
+    0x14: "MinaShop",
+    0x15: "OttoShop",
+    0x16: "Shopkeeper",
+    0x17: "Kronk",
+    0x18: "Blank",
+    0x19: "MinaG", # Used when meeting Garriott
+    0x1a: "OttoG", # Used when meeting Garriott
+    0x1b: "Garriott",
+    0x1d: "Warren",
+}
+
+
+def dump(story, table):
+    output = []
+    for i in range(0, 0x200, 2):
+        ptr = story[i] + (story[i+1] << 8)
+        if not ptr:
+            continue
+        output.append("<!-- event 0x%04x at 0x%04x -->\n" % (i, ptr))
+        while True:
+            byte = story[ptr]
+            if byte == 0x20:
+                # Half-width space
+                char = " "
+                ptr += 1
+            elif byte < 0x20 or byte >= 0xF0:
+                # Control code
+                try:
+                    char = cc[byte]
+                except KeyError:
+                    print("Unknown control code: %02x" % byte)
+                    char = "<%02x>" % byte
+                ptr += 1
+                if char == "<LS=" or char == "<RS=":
+                    char += speaker.get(story[ptr], "%02x" % story[ptr]) + ">"
+                    if story[ptr] not in speaker:
+                        print("Unknown speaker: %02x" % story[ptr])
+                    ptr += 1
+                elif char.endswith("="):
+                    char += "%02x" % story[ptr] + ">"
+                    ptr += 1
+            elif 0x21 <= byte <= 0x7F:
+                char = table[byte - 0x21]
+                ptr += 1
+            elif 0xA1 <= byte <= 0xDF:
+                char = table[byte - 0x20 - 0x21]
+                ptr += 1
+            else:
+                try:
+                    char = story[ptr : ptr + 2].decode("cp932")
+                except UnicodeDecodeError as e:
+                    print("???", hex(ptr))
+                    raise e
+                ptr += 2
+            output.append(char)
+            if byte == 0xFF:
+                break
+
+    with open("story.txt", "w") as of:
+        of.write("".join(output))
