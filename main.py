c95d08595ee4b6061fccbf34552eb0bae986f6bf
Lynn <lynn@foldr.moe>
2023-08-03 17:28:11 +0200

    Code to repack WSDATA.FCP and the floppy disk

--- main.py
+++ main.py
@@ -20,16 +20,26 @@
 # rom[0xc5000:0xc5400] = 0x400 bytes (what format?)
 # rom[0x2580:0x25a0] = b'WSSAVE00DAT \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xef\xba\xfcV\x07\x03\x00\x04\x00\x00'
 
+
+# oh god. the story is loaded into 0x7cb00 in memory,
+# but something else is loaded into 0x80b00 so it gets clobbered.
+# That means there's 0x4000 bytes for the story but we need 0x5188 bytes.
+
 from dataclasses import dataclass
 from typing import Dict, List
 import os.path
+import math
 import re
+import struct
 import sys
 
 from x86 import Patch
 import story
 import strings
 from util import edit, translate, find_strings
+
+
+FAT_START = 0x2400
 
 
 @dataclass
@@ -41,16 +51,74 @@ class Entry:
     data: bytes
 
 
+def pack_fcp(fcp: dict) -> bytes:
+    b = bytearray(b"FCP 0100")
+    p = 0x1208
+    for name, contents in fcp.items():
+        b += name.rstrip(b"@")
+        b += bytes([p >> 16 & 255, p >> 24 & 255, p & 255, p >> 8 & 255])
+        b += bytes([len(contents) & 255, len(contents) >> 8 & 255])
+        p += len(contents)
+    b = b.ljust(0x1208, b"\0")
+    for contents in fcp.values():
+        b += contents
+    return b
+
+
+def pack_fat_table(entries: list[int]) -> bytes:
+    b = bytearray(math.ceil(len(entries) / 2) * 3)
+    for i, x in enumerate(entries):
+        if i % 2 == 0:
+            b[i // 2 * 3] |= x & 0xFF
+            b[i // 2 * 3 + 1] |= x >> 8
+        else:
+            b[i // 2 * 3 + 1] |= (x & 0xF) << 4
+            b[i // 2 * 3 + 2] |= x >> 4
+    return bytes(b)
+
+
+def pack_fdi(fs: dict[bytes, any], rom: bytes) -> bytes:
+    b = bytearray(rom[:FAT_START])
+    sector = 2
+    queue = []
+
+    fat = [0xFFE, 0xFFF]
+
+    for i, (name, contents) in enumerate(fs.items()):
+        base, ext = name.encode().split(b".")
+        meta = rom[0x240B + 32 * i : 0x241A + 32 * i]
+        if i >= 12:
+            # Hack for save files
+            meta = b" \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xef\xba\xfcV"
+        se = sector.to_bytes(2, byteorder="little")
+        le = len(contents).to_bytes(4, byteorder="little")
+        b += base.ljust(8) + ext.ljust(3) + meta + se + le
+        queue.append((sector, contents))
+        old = sector
+        sector += math.ceil(len(contents) / 1024)
+        fat += [*range(old + 1, sector), 0xFFF]
+
+    ft = pack_fat_table(fat).ljust(0x800, b"\0")
+    b[0x1400:0x1C00] = ft
+    b[0x1C00:0x2400] = ft
+
+    for sector, contents in queue:
+        # Copy padding from ROM
+        b += rom[len(b) : (sector + 4) * 1024 + FAT_START]
+        b += contents
+    return b + rom[len(b) : len(rom)]
+
+
 if __name__ == "__main__":
     rom_name = "Winds_Seed.FDI"
-    # rom_name = "patched.fdi"
+    if "--dump-save" in sys.argv:
+        rom_name = "patched.fdi"
     with open(rom_name, "rb") as f:
         rom = bytearray(f.read())
 
     fs = {}
-    START = 0x2400
     for i in range(99):
-        header = rom[START + 32 * i : START + 32 * i + 32]
+        header = rom[FAT_START + 32 * i : FAT_START + 32 * i + 32]
         if header[0] == 0:
             break
         name = bytes(header[:8]).decode().strip()
@@ -59,12 +127,12 @@ if __name__ == "__main__":
             name += "." + ext
         sector = int.from_bytes(header[26:28], byteorder="little")
         length = int.from_bytes(header[28:32], byteorder="little")
-        start = (sector + 0x4) * 1024 + START
+        start = (sector + 0x4) * 1024 + FAT_START
         end = start + length
         data = rom[start:end]
-        fs[name] = Entry(i, name, start, end, data)
+        fs[name] = data
         # print(name, hex(start), hex(end))
-        # print(hex(START + 32 * i), header)
+        # print(hex(FAT_START + 32 * i), header)
         if not os.path.isdir("dump"):
             os.mkdir("dump")
         path = os.path.join("dump", name)
@@ -74,17 +142,16 @@ if __name__ == "__main__":
                 of.write(data)
 
     ws = fs["WS.COM"]
-    ws_patched = bytearray(ws.data)
+    ws_patched = bytearray(ws)
 
-    table = ws.data[0xCC40:0xCD80].decode("cp932")
+    table = ws[0xCC40:0xCD80].decode("cp932")
 
-    tl = story.parse()
-
-    fcp_file = bytearray(fs["WSDATA.FCP"].data)
+    fcp_file = bytearray(fs["WSDATA.FCP"])
     file_list = 0x8
     i = file_list
+    fcp = {}
     while fcp_file[i]:
-        name = bytes(fcp_file[i : i + 8].strip() + fcp_file[i + 8 : i + 12])
+        name = bytes(fcp_file[i : i + 12])
         address = (
             fcp_file[i + 13] << 24
             | fcp_file[i + 12] << 16
@@ -93,30 +160,31 @@ if __name__ == "__main__":
         )
         size = (fcp_file[i + 17] << 8) + fcp_file[i + 16]
         contents = fcp_file[address : address + size]
-        if name == b"STORY.DAT":
-            story_contents = contents
-            print("Original story length:", len(contents))
-            print("Translation length:", len(tl))
-            if len(tl) > len(contents):
-                print("That's too big!! Trimming it")
-                tl = tl[: len(contents)]
-            edit(fcp_file, address, tl)
-            # edit(fcp_file, address + 0x288, b"\x0d\x0b\x00\x0a\x02\x07\x0a")
-            # with open(name.decode(), "wb") as of:
-            #    of.write(contents)
+        while name in fcp:
+            name += b"@"
+        fcp[name] = contents
         i += 18
 
+    assert fcp_file == pack_fcp(fcp)
+    assert rom == pack_fdi(fs, rom)
+
+    original_story = fcp[b"STORY   .DAT"]
     if "--dump-story" in sys.argv:
         print("Dumping story...")
-        story.dump(story_contents, table)
+        story.dump(original_story, table)
+
+    print("Original story length:", len(original_story))
+    tl = story.parse()
+    print("Translation length:", len(tl))
+    fcp[b"STORY   .DAT"] = tl
 
     if "--find-strings" in sys.argv:
-        find_strings(ws.data)
+        find_strings(ws)
 
-    patched = rom[:]
+    # patched = rom[:]
 
     drbios = fs["DRBIOS.COM"]
-    bios_patched = bytearray(drbios.data)
+    bios_patched = bytearray(drbios)
 
     # Make jump over picture copy unconditional
     # edit(bios_patched, 0x9f7, b"\xeb")
@@ -195,6 +263,16 @@ if __name__ == "__main__":
     blank = double_menu_x.end
     Patch(ws_patched, 0x7302).call(double_menu_x.start)
 
+    # Copy more story (0x2900 words instead of 0x2000, i.e. 0x5200 bytes not 0x4000)
+    edit(ws_patched, 0x304, b"\xb9\x00\x29")
+
+    # CS:b7af is the DS value for story.
+    # CS:b7ad is the DS value for what comes after.
+    # (remember 2e 8e 1e af b7 is the MOV DS instruction for reading story)
+    # This ADD instruction adjusts the gap between them. We change it from 0x400 to 0x600.
+    # This really wins us 0x2000 bytes, not 0x200, because segment pointers express 16-byte increments.
+    edit(ws_patched, 0x18C, b"\x05\x00\x06")
+
     # Invert kanji pictures
     # edit(bios_patched, 0xa36, b"\x75")
 
@@ -202,24 +280,36 @@ if __name__ == "__main__":
     # edit(ws_patched, 0x2284, b"\x90\x90")
     # edit(ws_patched, 0x2293, b"\xb6\x00\x90")
 
-    if True:
-        for addr, ja, en in strings.ws:
-            try:
-                en.encode("ascii")
-            except UnicodeEncodeError as e:
-                print("Non-ASCII in English string:", en)
-                raise e
-            translate(ws_patched, addr, ja, en)
+    for addr, ja, en in strings.ws:
+        try:
+            en.encode("ascii")
+        except UnicodeEncodeError as e:
+            print("Non-ASCII in English string:", en)
+            raise e
+        translate(ws_patched, addr, ja, en)
 
-    patched[drbios.start : drbios.end] = bios_patched
-    patched[ws.start : ws.end] = ws_patched
-    fcp = fs["WSDATA.FCP"]
-    patched[fcp.start : fcp.end] = fcp_file
+    fs["DRBIOS.COM"] = bios_patched
+    fs["WS.COM"] = ws_patched
+    fs["WSDATA.FCP"] = pack_fcp(fcp)
+    with open("dump/WSSAVE00_lv1_forest.DAT", "rb") as f:
+        sav = bytearray(f.read())
 
-    patched[
-        0x2580:0x25A0
-    ] = b"WSSAVE00DAT \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xef\xba\xfcV\x07\x03\x00\x04\x00\x00"
-    patched[0xC5000:0xC5400] = open("dump/WSSAVE00_start.DAT", "rb").read()
+    # lvl, hp, maxhp, atk, def, spd
+    stats = struct.pack("<BHHHHH", 99, 999, 999, 999, 999, 999)
+
+    # Mina stats
+    sav[0x6A : 0x6A + 11] = stats
+    sav[0x9A:0x9C] = (9999).to_bytes(2, "little")
+    # Otto stats
+    sav[0xF0 : 0xF0 + 11] = stats
+
+    fs["WSSAVE00.DAT"] = sav
+    patched = pack_fdi(fs, rom)
+
+    # patched[
+    #     0x2580:0x25A0
+    # ] = b"WSSAVE00DAT \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xef\xba\xfcV\x07\x03\x00\x04\x00\x00"
+    # patched[0xC5000:0xC5400] = open("dump/WSSAVE01_start.DAT", "rb").read()
 
     with open("patched.fdi", "wb") as of:
         print("Writing patched.fdi")
